4007df28ac3ca215152837df3d922da1cb45cb64
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-02-06 09:14:45 +0530

    interface update

--- contracts/src/interfaces/IBridgeAdapter.sol
+++ contracts/src/interfaces/IBridgeAdapter.sol
@@ -7,7 +7,9 @@ interface IBridgeAdapter {
         address recipient,
         uint256 amount,
         uint256 destChainId
-    ) external returns (bytes32 bridgeTxId);
+    )
+        external
+        returns (bytes32 bridgeTxId);
 
     function estimateFee(uint256 destChainId, uint256 amount) external view returns (uint256);
 }
